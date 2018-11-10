d935e7068380b9172672d36b4d04c754c6bb735f
Natali Natali <2122nv@gmail.com>
2018-11-10 22:54:48 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/AutoTestRun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoTest;
+namespace ConsoleApp1
+{
+    class AutoTestRun
+    {
+        static void Main(string[] args)
+        {
+            FirstTest test = new FirstTest();
+            test.Test1();
+            Console.ReadLine();
+        }
+    }
+}
